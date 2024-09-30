b37bd76cdc68e2141c368880cf835bd670833b29
n-0773 <snoopy.basket2525@gmail.com>
2024-09-30 23:38:09 +0900

    Update ComprehensiveBeginner01_05.java

--- ComprehensiveBeginner01_05.java
+++ ComprehensiveBeginner01_05.java
@@ -1,3 +1,10 @@
+// 新規Java ファイル「ComprehensiveBeginner01_05.java」というファイルを作成し、
+// メインメソッドを書き、
+// 下記の通りコーディングしてください。
+
+// 1から100までの値を繰り返しで表示するが、3の倍数の時はfoo、5の倍数の時はbarと数字の代わりに表示するプログラムを作成してください。
+// 3と5の両方の倍数の時はfoobarと表示されるようにしてください。
+    
 class ComprehensiveBeginner01_05 {
     public static void main(String[] args) {
         
